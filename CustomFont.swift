3122c9ca99db362e66f411cd90992449fbd16546
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2026-02-02 19:50:08 +0300

    onboarding screen

---
+++ CustomFont.swift
@@ -0,0 +1,58 @@
+//
+//  CustomFont.swift
+//  Unitrack
+//
+//  Created by Sylus Abel on 02/02/2026.
+//
+
+import SwiftUI
+
+struct CustomFont: ViewModifier {
+    var textStyle: TextStyle
+    var name: String {
+        switch textStyle {
+        case .title:
+            return "Outfit SemiBold"
+        case .footnote:
+            return "Outfit Regular"
+        default:
+            return "Outfit Regular"
+        }
+    }
+    var size: CGFloat {
+        switch textStyle {
+        case .title:
+            return 24
+        case .footnote:
+            return 12
+        default:
+            return 22
+        }
+    }
+    var relative: Font.TextStyle {
+        switch textStyle {
+        case .title:
+            return .title
+        case .footnote:
+            return .footnote
+        default:
+            return .body
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        content.font(.custom(name, size: size, relativeTo: relative))
+    }
+}
+
+extension View {
+    func customFont(_ textStyle: TextStyle) -> some View {
+        modifier(CustomFont(textStyle: textStyle))
+    }
+}
+
+enum TextStyle {
+    case title
+    case body
+    case footnote
+}
--- Unitrack/OnboardingView.swift
+++ Unitrack/OnboardingView.swift
@@ -9,14 +9,54 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
-    
-    
-    private let riveViewModel = RiveViewModel(fileName: "shapes")
-    
-    
+    let button = RiveViewModel(fileName: "button")
+
     var body: some View {
-        
-        riveViewModel.view()
+        ZStack {
+            background
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Unified Portfolio Tracker")
+                    .font(.custom("Outfit SemiBold", size: 60, relativeTo: .largeTitle))
+                    .frame(width: 260, alignment: .leading)
+                
+                Text("Unitrack unifies tracking: real-time updates, custom real estate/fixed-income entries, alerts, premium risk analysis. One portfolio to track them all.")
+                    .customFont(.body)
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                button.view()
+                    .frame(width: 236, height: 64)
+                    .overlay(
+                        Label("Start Tracking", systemImage:
+                            "arrow.forward")
+                            .offset(x: 4, y: 4)
+                            .font(.headline)
+                    )
+                    .background(
+                        Color.black
+                            .cornerRadius(30)
+                            .blur(radius: 10)
+                            .opacity(0.3)
+                            .offset(y: 10)
+                    )
+                    .onTapGesture {
+                        button.play(animationName: "active")
+                    }
+                
+                Text(" Messy portfolios? Unify your assets with Unitrack.")
+                    .customFont(.footnote)
+                    .opacity(0.7)
+            }
+            .padding(40)
+            .padding(.top, 40)
+        }
+
+        }
+    var background: some View {
+        RiveViewModel(fileName: "shapes").view()
             .ignoresSafeArea()
             .blur(radius: 30)
             .background(
@@ -24,9 +64,8 @@ struct OnboardingView: View {
                     .blur(radius: 50)
                     .offset(x:200,y:100)
         )
-        
     }
-}
+    }
     
     struct OnboardingView_Preview: PreviewProvider {
         static var previews: some View {
