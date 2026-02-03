1aba7b7f61e8dc6842164fb08f0571664a849127
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2026-02-03 05:02:35 +0300

    Signin modal ui

---
+++ Unitrack/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  Unitrack
+//
+//  Created by Sylus Abel on 03/02/2026.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
---
+++ Unitrack/SigninView.swift
@@ -0,0 +1,18 @@
+//
+//  SigninView.swift
+//  Unitrack
+//
+//  Created by Sylus Abel on 03/02/2026.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SigninView()
+}
